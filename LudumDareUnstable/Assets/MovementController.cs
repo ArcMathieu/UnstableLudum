20f9bf00d166d25e876d8103ec1185e598e4c582
MrFrkl1 <55458843+MrFrkl1@users.noreply.github.com>
2021-10-03 16:35:26 +0200

    android
    touches mouvements tactiles

--- LudumDareUnstable/Assets/MovementController.cs
+++ LudumDareUnstable/Assets/MovementController.cs
@@ -7,6 +7,8 @@ public class MovementController : MonoBehaviour
     Rigidbody2D rb;
     Vector2 StartPos;
     Vector2 pos;
+    Vector3 touchPosition;
+    Vector3 direction;
     float HztVelocity;
 
     public float speed;
@@ -21,10 +23,23 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HztVelocity = Input.GetAxisRaw("Horizontal");
-        pos = rb.position;
-        pos.x = Mathf.Clamp(pos.x, StartPos.x - gap, StartPos.x + gap);
-        rb.velocity = new Vector2(HztVelocity * speed * Time.deltaTime, 0);
-        transform.position = pos;
+        if(Input.touchCount > 0)
+        {
+            //HztVelocity = Input.GetAxisRaw("Horizontal");
+            //rb.velocity = new Vector2(HztVelocity * speed * Time.deltaTime, 0);
+
+            pos = rb.position;
+            pos.x = Mathf.Clamp(pos.x, StartPos.x - gap, StartPos.x + gap);
+
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+            direction = (touchPosition - transform.position).normalized;
+            
+            rb.velocity = new Vector2(direction.x * speed * Time.deltaTime, 0);
+            transform.position = pos;
+
+            if (touch.phase == TouchPhase.Ended) rb.velocity = Vector2.zero;
+        }
     }
 }
