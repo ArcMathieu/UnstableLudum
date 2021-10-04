eb0d4a69f2cc42719920a23242f36dbe75cb9968
Wizerdy <grancru2000@gmail.com>
2021-10-04 16:21:50 +0200

    TMP + Proto cleaned

--- LudumDareUnstable/Assets/Scripts/GameManager.cs
+++ LudumDareUnstable/Assets/Scripts/GameManager.cs
@@ -2,55 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public static GameManager _instance;
 
+    public UIManager ui;
+
+    public GameObject firePanel;
     public GameObject lightPanel;
     public Coroutine blink;
 
     public bool running;
     public int waitTime;
 
-    public void StartBlink(float time, float numberOfBlinks)
-    {
-        if (blink != null)
-        {
+    private void Awake() {
+        if (GameManager._instance == null) {
+            _instance = this;
+            //DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    #region Blink
+
+    public void StartBlink(float time, float numberOfBlinks) {
+        if (blink != null) {
             StopCoroutine(blink);
         }
         blink = StartCoroutine(Blink(time, numberOfBlinks));
     }
 
-    public IEnumerator Blink(float time, float numberOfBlinks)
-    {
-        while (numberOfBlinks > 0)
-        {
+    public IEnumerator Blink(float time, float numberOfBlinks) {
+        while (numberOfBlinks > 0) {
             numberOfBlinks--;
             yield return new WaitForSeconds(time);
             lightPanel.SetActive(!lightPanel.activeSelf);
         }
-        if (lightPanel.activeSelf)
-        {
+        if (lightPanel.activeSelf) {
             lightPanel.SetActive(false);
         }
     }
 
-    private void Awake()
-    {
-        if (GameManager._instance == null)
-        {
-            _instance = this;
-            //DontDestroyOnLoad(this.gameObject);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
+    #endregion
+
+    public void Restart() {
+        Time.timeScale = 1f;
+        ui.DefeatScreenToggle(false);
     }
 
-
-    public void Lose()
-    {
-        //T'AS PERDU GROS NAZE
+    public void Lose() {
+        Time.timeScale = 0f;
+        ui.DefeatScreenToggle(false);
     }
 }
