df33a9d30affd4a64f024cb477fd2198557e57f8
MrFrkl1 <55458843+MrFrkl1@users.noreply.github.com>
2021-10-04 16:13:33 +0200

    Gyro

--- LudumDareUnstable/Assets/Scripts/Elements/Element.cs
+++ LudumDareUnstable/Assets/Scripts/Elements/Element.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public abstract class Element : MonoBehaviour {
+public abstract class Element : MonoBehaviour
+{
     public float fireStability = 1f;
     public float speed = 1f;
     public Element opposite = null;
@@ -14,43 +15,64 @@ public abstract class Element : MonoBehaviour {
     [SerializeField] private string _name = "";
     private Rigidbody2D _rb = null;
 
+    public Coroutine waitToDropCoroutine;
+    public float timeToDrop;
+
     #region Properties
-    public string Name {
-        get { return _name; } set { _name = value; }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
     }
     #endregion
 
-    private void Start() {
+    private void Start()
+    {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = new Vector2(0f, -speed);
         OnStart();
     }
 
-    private void Update() {
-        if (bounds != null && transform.position.y < bounds.position.y) {
+    private void Update()
+    {
+        if (bounds != null && transform.position.y < bounds.position.y)
+        {
             Delete();
         }
         OnUpdate();
     }
 
-    public void Delete() {
+    public void Delete()
+    {
         _rb.velocity = Vector2.zero;
         gameObject.SetActive(false);
         OnDelete();
         Destroy(gameObject);
     }
 
-    public void Retrieve() {
+    public void Retrieve()
+    {
         OnRetrieve();
         _rb.velocity = Vector2.zero;
         gameObject.SetActive(false);
     }
 
-    public void ChangeSpeed(float speed) {
+    public void ChangeSpeed(float speed)
+    {
         _rb.velocity = new Vector2(0f, -speed);
     }
-
-    protected virtual void OnStart() { }
+    protected virtual void OnStart()
+    {
+        if (waitToDropCoroutine != null)
+        {
+            StopCoroutine(waitToDropCoroutine);
+        }
+        waitToDropCoroutine = StartCoroutine(iWaitToDrop(timeToDrop));
+    }
+    public IEnumerator iWaitToDrop(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _rb.velocity = new Vector2(0f, -speed);
+    }
     protected virtual void OnUpdate() { }
     protected virtual void OnDelete() { }
     protected virtual void OnRetrieve() { }
